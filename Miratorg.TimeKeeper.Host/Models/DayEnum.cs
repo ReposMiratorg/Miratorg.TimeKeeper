323da5f0aac4eb90b0d61cb59ca899ff3c8d298b
Kluchnikov Sergey <s.kluchnikov@hotmail.com>
2024-05-02 10:32:15 +0200

    added dayheader

---
+++ Miratorg.TimeKeeper.Host/Models/DayEnum.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Miratorg.TimeKeeper.Host.Models;
+
+public enum DayEnum
+{
+    [Display(Name = "Пн")]
+    Monday = 1,
+    
+    [Display(Name = "Вт")]
+    Tuesday = 2,
+    
+    [Display(Name = "Ср")]
+    Wednesday = 3,
+
+    [Display(Name = "Чт")]
+    Thursday = 4,
+
+    [Display(Name = "Пт")]
+    Friday = 5,
+
+    [Display(Name = "Сб")]
+    Saturday = 6,
+
+    [Display(Name = "Вс")]
+    Sunday = 7
+}
