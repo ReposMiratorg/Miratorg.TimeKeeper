9a5f180e08d83e2dec73147c49467ff36e7a21c5
Sergey <35969211+KluchnikovSergey@users.noreply.github.com>
2024-05-07 23:13:59 +0300

    added migration

--- Miratorg.TimeKeeper.DataAccess/Entities/PlanEntity.cs
+++ Miratorg.TimeKeeper.DataAccess/Entities/PlanEntity.cs
@@ -7,10 +7,12 @@ public class PlanEntity : BaseEntity
     public DateTime Begin { get; set; }
     public DateTime End { get; set; }
     public PlanType PlanType { get; set; }
+    public Guid? StoreId { get; set; }
+    public virtual StoreEntity? Store { get; set; }
 }
 
 public enum PlanType
 {
     Plan = 1,
-    Overwork = 2,
+    Overwork = 2
 }
---
+++ Miratorg.TimeKeeper.DataAccess/Migrations/20240507201309_Update010.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Miratorg.TimeKeeper.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class Update010 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "StoreId",
+                table: "Plans",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Plans_StoreId",
+                table: "Plans",
+                column: "StoreId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Plans_Stores_StoreId",
+                table: "Plans",
+                column: "StoreId",
+                principalTable: "Stores",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Plans_Stores_StoreId",
+                table: "Plans");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Plans_StoreId",
+                table: "Plans");
+
+            migrationBuilder.DropColumn(
+                name: "StoreId",
+                table: "Plans");
+        }
+    }
+}
