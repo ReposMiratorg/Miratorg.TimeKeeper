9359e4dcad517f63752d03e0b38032a1e8142a27
Sergey <35969211+KluchnikovSergey@users.noreply.github.com>
2024-05-14 23:35:20 +0300

    made color

---
+++ Miratorg.TimeKeeper.Host/Components/Shared/ColorConfig.cs
@@ -0,0 +1,11 @@
+﻿namespace Miratorg.TimeKeeper.Host.Components.Shared;
+
+public static class ColorConfig
+{
+    public const string BackgroundDefault = "#ced4da";
+    public const string BackgroundPlan = "#2c8ef8";
+    public const string BackgroundOverWork = "#04533c";
+    public const string Background1C = "#0acf97";
+    public const string BackgroundScud = "#ffc35a";
+    public const string BackgroundFActOverWork = "#ff679b";
+}
