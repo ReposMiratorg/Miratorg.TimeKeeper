fa5695cf4112bdf036153ac1b8df9bf83de32d77
Sergey <35969211+KluchnikovSergey@users.noreply.github.com>
2024-05-19 23:14:37 +0300

    added mingation

---
+++ Miratorg.TimeKeeper.DataAccess/Migrations/20240519201319_Update020.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Miratorg.TimeKeeper.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class Update020 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "StoreId",
+                table: "Plans",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "Guid1C",
+                table: "Employees",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Guid1C",
+                table: "Employees");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "StoreId",
+                table: "Plans",
+                type: "uniqueidentifier",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+        }
+    }
+}
