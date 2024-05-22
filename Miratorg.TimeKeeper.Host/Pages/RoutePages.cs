00f168133a5adba92547f3a038a11a8cd64162f5
Sergey <35969211+KluchnikovSergey@users.noreply.github.com>
2024-05-22 03:03:37 +0300

    update 0.1.10

--- Miratorg.TimeKeeper.Host/Pages/RoutePages.cs
+++ Miratorg.TimeKeeper.Host/Pages/RoutePages.cs
@@ -5,5 +5,6 @@ public class RoutePages
     public const string HOME = "/";
     public const string SCHEDULE = "/schedule";
     public const string STORE_LIMIT = "/store-limit";
+    public const string STORE_LIMIT_2 = "/store-limit-2";
     public const string WORK_CUSTOM = "/work-custom";
 }
