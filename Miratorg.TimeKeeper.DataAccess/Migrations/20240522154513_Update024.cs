d89f9f7d19270f9f714033be44b9a977897671e4
Sergey <35969211+KluchnikovSergey@users.noreply.github.com>
2024-05-22 18:45:36 +0300

    added migration

---
+++ Miratorg.TimeKeeper.DataAccess/Migrations/20240522154513_Update024.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Miratorg.TimeKeeper.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class Update024 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "HourLimit",
+                table: "Stores");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "HourLimit",
+                table: "Stores",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
