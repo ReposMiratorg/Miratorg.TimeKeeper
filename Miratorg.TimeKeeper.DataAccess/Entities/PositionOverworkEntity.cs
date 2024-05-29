e93804c73488607f4453ba47310d565126048ac6
Sergey <35969211+KluchnikovSergey@users.noreply.github.com>
2024-05-30 01:20:51 +0300

    temp

---
+++ Miratorg.TimeKeeper.DataAccess/Entities/PositionOverworkEntity.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Miratorg.TimeKeeper.DataAccess.Entities;
+
+public class PositionOverworkEntity : BaseEntity
+{
+    public string Name { get; set; }
+    public Guid TypeOverWorkEntityId { get; set; }
+
+    [ForeignKey("TypeOverWorkEntityId")]
+    public virtual TypeOverWorkEntity TypeOverWorkEntity { get; set; }
+}
--- Miratorg.TimeKeeper.DataAccess/Entities/TypeOverWorkEntity.cs
+++ Miratorg.TimeKeeper.DataAccess/Entities/TypeOverWorkEntity.cs
@@ -9,12 +9,3 @@ public class TypeOverWorkEntity : BaseEntity
 
     public virtual List<PositionOverworkEntity> PositionOverworks { get; set; }
 }
-
-public class PositionOverworkEntity : BaseEntity
-{
-    public string Name { get; set; }
-    public Guid TypeOverWorkEntityId { get; set; }
-
-    [ForeignKey("TypeOverWorkEntityId")]
-    public virtual TypeOverWorkEntity TypeOverWorkEntity { get; set; }
-}
