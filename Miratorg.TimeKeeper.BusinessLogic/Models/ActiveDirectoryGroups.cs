ea505cccbcceb438a9bf85058c1480bf74526cfa
Kluchnikov Sergey <s.kluchnikov@hotmail.com>
2024-06-03 21:37:17 +0200

    added ad groups

---
+++ Miratorg.TimeKeeper.BusinessLogic/Models/ActiveDirectoryGroups.cs
@@ -0,0 +1,8 @@
+﻿namespace Miratorg.TimeKeeper.BusinessLogic.Models;
+
+public class ActiveDirectoryGroups
+{
+    public const string RY= "WG RY BI 00000";
+    public const string Supermarkets = "WG Supermarkets 00000";
+    public const string HR = "WG HR Managment 00000";
+}
