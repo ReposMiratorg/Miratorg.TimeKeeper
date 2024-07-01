d8f1cdf509350bf4179faa79ee3feb0cd9123333
Kluchnikov Sergey <s.kluchnikov@hotmail.com>
2024-07-01 21:48:23 +0200

    added test

---
+++ Miratorg.TimeKeeper.Tests/v4/IBlockingConfigTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Configuration;
+using Miratorg.TimeKeeper.BusinessLogic.Configs;
+using Moq;
+
+namespace Miratorg.TimeKeeper.Tests.v4;
+
+public class IBlockingConfigTests
+{
+    private Mock<IBlockingConfig> mockBlockingConfig = new Mock<IBlockingConfig>();
+
+    public IBlockingConfigTests()
+    {
+        mockBlockingConfig.SetupGet(m => m.IsUse).Returns(true);
+        mockBlockingConfig.SetupGet(m => m.Dates).Returns(new int[] { 1, 16 });
+        mockBlockingConfig.SetupGet(m => m.Hour).Returns(8);
+    }
+
+    [Fact]
+    public void CheckConfig()
+    {
+        // Arrange
+        var blockingConfig = mockBlockingConfig.Object;
+
+        // Act
+        var result = blockingConfig.ChechAccess(DateTime.Now);
+
+        // Assert
+        Assert.True(blockingConfig.IsUse);
+
+        // Act
+
+        // Assert
+        Assert.True(blockingConfig.IsUse);
+        Assert.Equal(8, blockingConfig.Hour);
+        Assert.Equal(1, blockingConfig.Dates[0]);
+        Assert.Equal(16, blockingConfig.Dates[1]);
+    }
+}
