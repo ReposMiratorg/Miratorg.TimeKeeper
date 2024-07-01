6918cf3f2faa93d30892a55e159a95a4ba845d19
Kluchnikov Sergey <s.kluchnikov@hotmail.com>
2024-07-02 00:47:50 +0200

    test

--- Miratorg.TimeKeeper.BusinessLogic/Configs/BlockingConfig.cs
+++ Miratorg.TimeKeeper.BusinessLogic/Configs/BlockingConfig.cs
@@ -10,7 +10,6 @@ public class BlockingConfig
 
 public interface IBlockingService
 {
-    bool ChechAccess(DateTime date);
     bool IsBlockedDay(DateTime calendarDay, DateTime now);
 }
 
@@ -18,48 +17,9 @@ public class BlockingService : IBlockingService
 {
     private readonly BlockingConfig blockingConfig;
 
-    private bool IsUse = false;
-    private int[] Dates = new int[0];
-    private int Hour = 8;
-
     public BlockingService(IOptions<BlockingConfig> config)
     {
         blockingConfig = config.Value;
-    }
-
-    public bool ChechAccess(DateTime inputDate)
-    {
-        if (IsUse == false)
-        {
-            return true;
-        }
-
-        var currentDate = DateTime.Now;
-        inputDate = new DateTime(inputDate.Year, inputDate.Month, inputDate.Day, currentDate.Hour, currentDate.Minute, 0);
-
-        var blockDates = new List<DateTime>();
-        foreach (var item in Dates)
-        {
-            blockDates.Add(new DateTime(currentDate.Year, currentDate.Month, item, Hour, 0, 0));
-        }
-
-        foreach (var blockDate in blockDates)
-        {
-            if (blockDate.Date == inputDate.Date)
-            {
-                if (Hour <= currentDate.Hour)
-                {
-                    return false;
-                }
-            }
-
-            if (blockDate >= inputDate)
-            {
-                return false;
-            }
-        }
-
-        return true;
     }
 
     public bool IsBlockedDay(DateTime calendarDay, DateTime now)
